11846fc7b400d6b446ac9cd2bc087d5011f2761b
iamthanuj <thanujdevil@gmail.com>
2024-02-19 19:40:29 +0530

    UI chnages

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Login() {
+function Login({ logToggle }) {
   return (
     <div>
       <form className="max-w-md mx-auto font-inter">
@@ -14,7 +14,7 @@ function Login() {
             required
           />
           <label
-            for="floating_email"
+            htmlFor="floating_email"
             className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
           >
             Email address
@@ -30,7 +30,7 @@ function Login() {
             required
           />
           <label
-            for="floating_password"
+            htmlFor="floating_password"
             className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
           >
             Password
@@ -50,7 +50,7 @@ function Login() {
         <button
           className="text-secendoryColor font-bold"
           onClick={() => {
-            onShow("register");
+            logToggle(true);
           }}
         >
           Create one
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Register from '../components/Register'
 import Login from '../components/Login'
 import ColorLogo from '../assets/colorLogo.png'
 
 function LoginPage() {
+
+  const [loginToggle, setLogginToggle] = useState(false);
+
+  const loginScreenChange = (key)=>{
+    if(key){
+      setLogginToggle(!loginToggle)
+    }
+  }
+
   return (
     <div className='font-inter'>
         <div className='bg-landing-bg h-screen flex justify-center items-center'>
@@ -12,10 +21,8 @@ function LoginPage() {
                 <div className='mb-3'>
                     <img src={ColorLogo} alt="logo-image" className='w-72 mx-auto mb-3' />
                     <p className='text-center font-bold text-xl'>Connect With Unimate</p>
-                </div>
-                {/* <Login/> */}
-                <Register/>
-
+                </div> 
+                {loginToggle ? <Register logToggle={loginScreenChange} /> : <Login  logToggle={loginScreenChange} />}
             </div>
         </div>
       </div>
